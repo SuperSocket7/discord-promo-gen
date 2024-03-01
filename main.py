7895e540b2621e3d5bb29fb46c2f63faa4b277d0
風緑 <mail@nekokawa.net>
2024-03-01 21:19:37 +0900

    細かい変更

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import asyncio
 import aiohttp
 import sys
-import uuid
 import json
 
 try:
     threads = int(sys.argv[1])
 except IndexError:
     threads = 4
+
+# Opera GXのアカウントトークンをここに入れよ
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhOThlNmNmZC0yYmE3LTQ3MDAtYjZkNy0wOTIzNDBhMjIzYjQiLCJleHAiOjE3MDkyOTU3MDF9.JYBmMg-_WsZYBjkrhnaMMBnmW6AMQqfnC1fWiRhAMZLBLKLYvnafAcZPVfBHdTbCTobQ8kaU3BE6GF672iBnfeikEJ0qj5sEAZVkbyHll3ySU3rUiuDbT2mQl9nwrDu_rJg8z-TduufZlxKVcdu3x1q3sVUpZRUGvC07hEGusg-nt1ChLWVuBz_4dRT4WVblNH95hR25T8lG-0DD3Ln1aiwbAhX6uggH4zQGhKGeiPIVxVP7COx_-8gaCd7NAPeH62jnvGjD5-EJ9EJp1NkseL7EdmebZufObVJ8IXjKgRQzBzCpOwP_3h3uNFydrFAReHSD4-ILf0I8FJzHYsDk_yIrCTbP-9YOrgaprov0GfQeghomZUsxtt8r0oqAmujZ_K8ndvhRRu735rru_BMcAGTIIzym3fpOH4LUEQwGab55RYykPzHQG3VzOsSNQbnng5dWTRALiFh9J4LzoaH31JIm0Y8T-xi_AvtA5VCNYglBFnzJrDGIOH0E8zBpgv_NST8P_AMyBrbTbYeDQf4K15c1xpf7JkQyx4kSaBCPDEB7QPZvAi_wDOgA-ait2Zt_Q0O7OylK624NvepAUOiQLmEDlmFjrAPwOzlzJFmX1eybmsKx0WFNYC7H5U6avnBqEJLFKjtugw1E2VjxEFPNPUNI4gB-5adjN_BoTUqLWyg"
+
 headers = {
-    'authority': 'api.discord.gx.games',
+    'authority': 'discord.opr.gg',
     'accept': '*/*',
     'accept-language': 'en-US,en;q=0.9',
-    'content-type': 'application/json',
+    'authorization': token,
     'origin': 'https://www.opera.com',
     'referer': 'https://www.opera.com/',
     'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Opera GX";v="106"',
@@ -27,12 +30,9 @@ headers = {
 
 async def runner():
     while True:
-        data = {
-            "partnerUserId": str(uuid.uuid4())
-        }
         try:
             async with aiohttp.ClientSession(headers=headers) as session:
-                async with session.post("https://api.discord.gx.games/v1/direct-fulfillment", json=data) as r:
+                async with session.post("https://discord.opr.gg/v2/direct-fulfillment") as r:
                     if r.status == 200:
                         res = json.loads(await r.text())
                         promo_url = f"https://discord.com/billing/partner-promotions/1180231712274387115/{res['token']}"
