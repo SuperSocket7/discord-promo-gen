f19d7ccbd8a45e8fc60e6b852f4630e20289b60c
風緑 <mail@nekokawa.net>
2024-03-10 19:56:25 +0900

    proxy対応

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import aiohttp
 import requests
 import sys
 import json
+import os
+import random
 from http.cookiejar import MozillaCookieJar
 
 try:
@@ -26,36 +28,52 @@ headers = {
 }
 cj = MozillaCookieJar('cookies.txt')
 cj.load()
-# aiohttpでCookieを使う方法が難解です
-r = requests.get("https://api.gx.me/profile/token", headers=headers, cookies=cj)
-token = r.json()["data"]
-headers["authorization"] = token
+if os.path.exists("proxies.txt"):
+    with open("proxies.txt", "r") as f:
+        proxies = f.read().splitlines()
+else:
+    proxies = None
+
+
+def generate_token():
+    # aiohttpでCookieを使う方法が難解です
+    r = requests.get("https://api.gx.me/profile/token", headers=headers, cookies=cj)
+    token = r.json()["data"]
+    headers["authorization"] = token
 
 
 async def runner():
     while True:
         try:
             async with aiohttp.ClientSession(headers=headers) as session:
-                async with session.post("https://discord.opr.gg/v2/direct-fulfillment") as r:
-                    if r.status == 200:
-                        res = json.loads(await r.text())
-                        promo_url = f"https://discord.com/billing/partner-promotions/1180231712274387115/{res['token']}"
-                        with open("promos.txt", "a") as f:
-                            f.write(f"{promo_url}\n")
-                        print(f"[ \x1b[32mOK\x1b[0m ] {promo_url}")
-                    elif r.status == 401:
-                        print("[ \x1b[31mFAILED\x1b[0m ] 認証失敗")
-                    elif r.status == 403:
-                        print("[ \x1b[31mFAILED\x1b[0m ] なぞのエラー")
-                    elif r.status == 429:
-                        print("[ \x1b[31mFAILED\x1b[0m ] レート制限がっ")
-                    else:
-                        print("[ \x1b[31mFAILED\x1b[0m ] 知らないエラーだ")
+                if proxies:
+                    proxy = random.choice(proxies)
+                    r = await session.post("https://discord.opr.gg/v2/direct-fulfillment", proxy=proxy)
+                else:
+                    r = await session.post("https://discord.opr.gg/v2/direct-fulfillment")
+                if r.status == 200:
+                    res = json.loads(await r.text())
+                    promo_url = f"https://discord.com/billing/partner-promotions/1180231712274387115/{res['token']}"
+                    with open("promos.txt", "a") as f:
+                        f.write(f"{promo_url}\n")
+                    print(f"[ \x1b[32mOK\x1b[0m ] {promo_url}")
+                elif r.status == 401:
+                    print(f"[ INFO ] トークンが失効したぽいから再生成するぞ")
+                    # トークン再生成
+                    generate_token()
+                    continue
+                elif r.status == 403:
+                    print("[ \x1b[31mFAILED\x1b[0m ] なぞのエラー")
+                elif r.status == 429:
+                    print("[ \x1b[31mFAILED\x1b[0m ] レート制限がっ")
+                else:
+                    print("[ \x1b[31mFAILED\x1b[0m ] 知らないエラーだ")
         except Exception as e:
             print(f"[ \x1b[31mFAILED\x1b[0m ] 知らないエラーだ {e}")
 
 
 async def main():
+    generate_token()
     i = 0
     coroutines = []
     while i < threads:
